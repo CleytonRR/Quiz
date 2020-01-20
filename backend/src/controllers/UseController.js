4b8b3b53bdd43068ee59d1f43cfe1b9e695a5cbd
cleytonRR <cleytonrodrigues69@live.com>
2020-01-20 19:05:06 -0300

    Created method show in UserController

--- backend/src/controllers/UseController.js
+++ backend/src/controllers/UseController.js
@@ -1,6 +1,14 @@
 const User = require('../model/UserModel')
 
 module.exports = {
+  async show (req, res) {
+    try {
+      const response = await User.findAll()
+      res.json(response)
+    } catch (error) {
+      console.error('Erro in find users', +error)
+    }
+  },
   async store (req, res) {
     const { name } = req.body
     try {
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ const AskController = require('./controllers/AskController')
 const routes = Router()
 
 routes.post('/user', UserController.store)
+routes.get('/user', UserController.show)
 routes.post('/ask', AskController.store)
 
 module.exports = routes
