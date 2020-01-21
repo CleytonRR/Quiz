df06352d6dfb0803dacb1d77235ebfb4c3764d37
cleytonRR <cleytonrodrigues69@live.com>
2020-01-21 14:52:07 -0300

    Created test for ensure creation of ask

--- backend/src/tests/userAskTest/askController.test.js
+++ backend/src/tests/userAskTest/askController.test.js
@@ -7,11 +7,23 @@ var askMock = {
   ask: 'Quem criou o teorema de pitagoras'
 }
 
+var askMockCreate = {
+  ask: 'Quem idealizou o Calculo?',
+  answer: 'Newton',
+  choices: 'Newton, Platão, Aristoteles, Gauss'
+}
+
 describe('Suite test for Ask Controll', function () {
-  it('Ensure the return of a list of questions', async () => {
+  it('GET/ask -> Ensure the return of a list of questions', async () => {
     const response = await request(app).get('/ask')
     assert.ok(response.statusCode === 200)
     assert.ok(Array.isArray(response.body) === true)
     assert.deepStrictEqual(askMock.ask, response.body[0].ask)
+  })
+
+  it.only('POST/ask -> Ensure creation of new asks', async () => {
+    const response = await request(app).post('/ask').send(askMockCreate).set('Accept', 'applicatin/json')
+    assert.deepStrictEqual(askMockCreate.ask, response.body.ask)
+    assert.ok(response.statusCode === 200)
   })
 })
