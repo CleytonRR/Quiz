2bef182339aa70bb2cec156c03027af372e5988f
cleytonRR <cleytonrodrigues69@live.com>
2020-01-22 17:06:47 -0300

    deleted unused code

--- web/src/App.js
+++ web/src/App.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import UserView from './components/Users/index'
-// import api from './services/api'
-// import { useState } from 'react';
-// import { useEffect } from 'react';
+
 
 function App() {
-  // const [users, setUsers] = useState([])
 
-  // useEffect(() => {
-  //   async function loadUsers() {
-  //     const response = await api.get('user')
-
-  //     setUsers(response.data)
-  //   }
-  //   loadUsers()
-    
-  // }, [])
   return (
     <UserView />
   );
