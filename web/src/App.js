ee5ed26e40144a22e89817dc6859d5161fc749c0
cleytonRR <cleytonrodrigues69@live.com>
2020-01-22 17:04:52 -0300

    Refactor in component

--- web/src/App.js
+++ web/src/App.js
@@ -1,29 +1,23 @@
 import React from 'react';
-// import UserView from './components/Users/index'
-import api from './services/api'
-import { useState } from 'react';
-import { useEffect } from 'react';
+import UserView from './components/Users/index'
+// import api from './services/api'
+// import { useState } from 'react';
+// import { useEffect } from 'react';
 
 function App() {
-  const [users, setUsers] = useState([])
+  // const [users, setUsers] = useState([])
 
-  useEffect(() => {
-    async function loadUsers() {
-      const response = await api.get('user')
+  // useEffect(() => {
+  //   async function loadUsers() {
+  //     const response = await api.get('user')
 
-      setUsers(response.data)
-    }
-    loadUsers()
+  //     setUsers(response.data)
+  //   }
+  //   loadUsers()
     
-  }, [])
+  // }, [])
   return (
-  <ul>
-    {users.map(user => (
-    <li key={user.id}>
-      <p>nome: {user.name}</p>
-    </li>
-    ))}
-  </ul>
+    <UserView />
   );
 }
 
--- web/src/components/Users/index.js
+++ web/src/components/Users/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import api from '..//../services/api'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function UserView() {
-    return <h1>Olha mqqqudei e continuo funcinando</h1>
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        async function loadUser() {
+            const response = await api.get('user')
+            setUsers(response.data)
+        }
+
+        loadUser()
+    }, []) 
+    return (
+        <ul>
+            {users.map(user => (
+                <li key={user.id}>
+                    <p>nome: {user.name}</p>
+                </li>
+            ))}
+        </ul>
+    )
   }
 
 export default UserView
