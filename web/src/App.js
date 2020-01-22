db2df2e24d4f50bbe6e822034fc9f43d85b57242
cleytonRR <cleytonrodrigues69@live.com>
2020-01-22 16:59:23 -0300

    Create struct for application web and added cors on backend

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+// import UserView from './components/Users/index'
+import api from './services/api'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get('user')
+
+      setUsers(response.data)
+    }
+    loadUsers()
+    
+  }, [])
   return (
-    <h1>Estamos começando</h1>
+  <ul>
+    {users.map(user => (
+    <li key={user.id}>
+      <p>nome: {user.name}</p>
+    </li>
+    ))}
+  </ul>
   );
 }
 
---
+++ web/src/components/Users/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function UserView() {
+    return <h1>Olha mqqqudei e continuo funcinando</h1>
+  }
+
+export default UserView
