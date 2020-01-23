ad1dfe31c8066ad9269fbcabd2eef55b35bd0341
cleytonRR <cleytonrodrigues69@live.com>
2020-01-23 16:21:42 -0300

    Initial Desing

---
+++ web/src/components/asks/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button, Form} from 'react-bootstrap'
+
+function Ask() {
+    return (
+        <Form>
+            <Button className='btn-outline-primary'></Button>
+        </Form>
+    )
+}
+
+export default Ask
