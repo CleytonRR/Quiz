8cb34888eb2dd195c09f30e81512849349c040f1
cleytonRR <cleytonrodrigues69@live.com>
2020-01-23 17:30:05 -0300

    Querying database questions

--- web/src/components/asks/index.js
+++ web/src/components/asks/index.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import api from '../../services/api'
 import { Button, Form, Jumbotron } from 'react-bootstrap'
 
 import './styles.css'
 
 function Ask() {
+    const [asks, setAsks] = useState([])
+
+    useEffect(() => {
+        async function getAsks() {
+            const response = await api.get('ask')
+            setAsks(response.data)
+        }
+
+        getAsks()
+    }, [])
     function asnwerClick(e) {
         e.preventDefault()
         console.log(e.target.innerText)
@@ -12,11 +23,14 @@ function Ask() {
     return (
         <Jumbotron fluid className='question'>
             <Form>
-                <h3 className='text-center'>Quem descobriu o teorema de pitagoras</h3>
-                <Button variant="outline-primary" block onClick={asnwerClick}>Platão</Button>
-                <Button variant="outline-primary" block onClick={asnwerClick}>Aristoteles</Button>
-                <Button variant="outline-primary" block onClick={asnwerClick}>Tales</Button>
-                <Button variant="outline-primary" block onClick={asnwerClick}>Pitagoras</Button>
+                {asks.map(ask => (
+                    <>
+                        <h3 className='text-center' key={ask.id}>Quem descobriu o teorema de pitagoras</h3>
+                        {ask.choices.split(',').map((choice,index) => (
+                            <Button key={index} variant="outline-primary" block onClick={asnwerClick}>{choice}</Button>
+                        ))}
+                    </>
+                ))}
             </Form>
         </Jumbotron>
     )
