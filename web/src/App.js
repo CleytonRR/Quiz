a14a2040d4b8133ef317f3f8e13139c1dd937ebe
cleytonRR <cleytonrodrigues69@live.com>
2020-01-22 22:26:04 -0300

    unnecessary deleted files

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import UserView from './components/Users/index'
-import { Jumbotron } from 'react-bootstrap' 
+import { Jumbotron, Container } from 'react-bootstrap' 
 
 import './static/css/global.css'
 
 function App() {
 
   return (
-    <div id='app'>
+    <Container className='container-fluid'>
       <Jumbotron>
-      <h1>Quiz matemático</h1>
+      <h1 className='text-center'>Quiz matemático</h1>
       <UserView />
       </Jumbotron>
-    </div>
+    </Container>
   );
 }
 
