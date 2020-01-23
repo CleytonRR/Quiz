9c194a5ca3f49dd117a4309c43e28c3baa592ac7
cleytonRR <cleytonrodrigues69@live.com>
2020-01-23 16:52:40 -0300

    Event listener

--- web/src/components/asks/index.js
+++ web/src/components/asks/index.js
@@ -4,14 +4,19 @@ import { Button, Form, Jumbotron } from 'react-bootstrap'
 import './styles.css'
 
 function Ask() {
+    function asnwerClick(e) {
+        e.preventDefault()
+        console.log(e.target.innerText)
+    }
+
     return (
         <Jumbotron fluid className='question'>
             <Form>
                 <h3 className='text-center'>Quem descobriu o teorema de pitagoras</h3>
-                <Button variant="outline-primary" block>Platão</Button>
-                <Button variant="outline-primary" block>Aristoteles</Button>
-                <Button variant="outline-primary" block>Tales</Button>
-                <Button variant="outline-primary" block>Pitagoras</Button>
+                <Button variant="outline-primary" block onClick={asnwerClick}>Platão</Button>
+                <Button variant="outline-primary" block onClick={asnwerClick}>Aristoteles</Button>
+                <Button variant="outline-primary" block onClick={asnwerClick}>Tales</Button>
+                <Button variant="outline-primary" block onClick={asnwerClick}>Pitagoras</Button>
             </Form>
         </Jumbotron>
     )
