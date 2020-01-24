127fd897539eae3efe55823e15ae05cdabb4e94b
cleytonRR <cleytonrodrigues69@live.com>
2020-01-24 19:44:17 -0300

    Transforming components into class

--- web/src/pages/initialPage/index.js
+++ web/src/pages/initialPage/index.js
@@ -2,15 +2,17 @@ import React from 'react'
 import { Jumbotron, Container } from 'react-bootstrap'
 import InitialComponent from '../../components/InitialComponent'
 
-function InitalPage() {
-    return (
-        <Container className='container-fluid'>
-            <Jumbotron>
-                <h1 className='text-center'>Quiz matemático</h1>
-                <InitialComponent />
-            </Jumbotron>
-        </Container>
-    )
+class InitalPage extends React.Component {
+    render() {
+        return (
+            <Container className='container-fluid'>
+                <Jumbotron>
+                    <h1 className='text-center'>Quiz matemático</h1>
+                    <InitialComponent />
+                </Jumbotron>
+            </Container>
+        )
+    }
 }
 
 export default InitalPage
