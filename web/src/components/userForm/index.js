6310730a6c5b3859a06b903f240d1d20694ea7e1
cleytonRR <cleytonrodrigues69@live.com>
2020-01-24 20:58:56 -0300

    Refactor component function for compenent class

--- web/src/components/userForm/index.js
+++ web/src/components/userForm/index.js
@@ -1,16 +1,27 @@
 import React from 'react'
 
-function FormCreation() {
-    return (
-        <form>
-            <div className='form-group'>
-                <label htmlFor="name">Nome</label>
-                <input type="text" className='form-control' id='name' aria-describedby="nameUser" />
-                <small id='nameUser' className='form-text text-muted'>Insira seu nome para iniciar o desafio</small>
-            </div>
-            <button type="submit" class="btn btn-primary">Começar</button>
-        </form>
-    )
+
+class FormCreation extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            rendering: false
+        }
+    }
+    render() {
+        return (
+            <form>
+                <div className='form-group'>
+                    <label htmlFor="name">Nome</label>
+                    <input type="text" className='form-control' id='name' aria-describedby="nameUser" />
+                    <small id='nameUser' className='form-text text-muted'>Insira seu nome para iniciar o desafio</small>
+                </div>
+                <button type="submit" class="btn btn-primary">Começar</button>
+            </form>
+        )
+
+    }
 }
+
 
 export default FormCreation
--- web/src/pages/initialPage/index.js
+++ web/src/pages/initialPage/index.js
@@ -3,6 +3,7 @@ import { Jumbotron, Container } from 'react-bootstrap'
 import InitialComponent from '../../components/InitialComponent'
 
 class InitalPage extends React.Component {
+
     render() {
         return (
             <Container className='container-fluid'>
