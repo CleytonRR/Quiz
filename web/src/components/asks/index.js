0b24a0a029e1d66883a997ecb4a3b1a10772e9e2
cleytonRR <cleytonrodrigues69@live.com>
2020-01-27 18:08:57 -0300

    Refactor to class component

--- web/src/components/asks/index.js
+++ web/src/components/asks/index.js
@@ -1,53 +1,80 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import api from '../../services/api'
 import Fade from 'react-reveal/Fade'
 import { Button, Form, Jumbotron } from 'react-bootstrap'
 
 import './styles.css'
 
-function Ask() {
-    const [asks, setAsks] = useState([])
-    const [rendering, setRendering] = useState(false)
 
-    useEffect(() => {
+class Ask extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            asks: []
+        }
+    }
+    componentDidMount() {
         async function getAsks() {
             const response = await api.get('ask')
-            setAsks(response.data)
+            this.setState({
+                asks: response.data
+            })
         }
-        getAsks()
-    }, [rendering])
-    function asnwerClick(e) {
-        e.preventDefault()
-        if (e.target.innerText === asks[e.target.value].answer) {
-            e.target.className = 'btn btn-success btn-block'
-            setRendering(!rendering)
-        } else {
-            e.target.className = 'btn btn-warning btn-block'
+        if (this.state.asks === 0) {
+            console.log("Entrei aqui")
+            getAsks()
         }
     }
 
-    return (
-        <Fade left big >
-            <Jumbotron fluid className='question'>
-                <Form>
-                    {asks.map((ask, indexAsk) => (
-                        <>
-                            <h3 className='text-center' key={ask.id}>Quem descobriu o teorema de pitagoras</h3>
-                            {ask.choices.split(',').map((choice, index) => (
-                                <Button
-                                    value={indexAsk}
-                                    key={index}
-                                    variant="primary"
-                                    block
-                                    onClick={asnwerClick}>{choice}
-                                </Button>
+    componentWillUnmount() {
+        if(this.state.asks){
+            
+        }
+    }
+
+    question() {
+        async function getAsks() {
+            const response = await api.get('ask')
+            this.setState({
+                asks: response.data
+            })
+        }
+        getAsks()
+        alert('oie')
+        console.log(this.state.asks)
+        return (
+            <h1>{this.state.asks[0].ask}</h1>
+        )
+    }
+
+    render() {
+        if (this.state.asks === null || this.state.asks.length === 0) {
+            return <h1>Estou aqui</h1>
+        } else {
+            return (
+                <Fade left big >
+                    <Jumbotron fluid className='question'>
+                        <Form>
+                            {this.state.asks.map((ask, index) => (
+                                <>
+                                    <h3 className='text-center' key={index}>{this.question()}</h3>
+                                    {ask.choices.split(',').map((choice, index) => (
+                                        <Button
+                                            value={ask.id}
+                                            key={index}
+                                            variant="primary"
+                                            block>{choice}
+                                        </Button>
+                                    ))}
+                                </>
                             ))}
-                        </>
-                    ))}
-                </Form>
-            </Jumbotron>
-        </Fade>
-    )
+                        </Form>
+                    </Jumbotron>
+                </Fade>
+            )
+
+        }
+    }
 }
 
 export default Ask
