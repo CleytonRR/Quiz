93402e07e10276c0571d9bccbb7154bc60eb89ef
cleytonRR <cleytonrodrigues69@live.com>
2020-01-30 00:27:26 -0300

    Refactor to class

--- web/src/components/Users/index.js
+++ web/src/components/Users/index.js
@@ -1,45 +1,53 @@
 import React from 'react';
 import api from '..//../services/api'
-import { useState } from 'react';
-import { useEffect } from 'react';
 
 import './styles.css'
 
-function UserView() {
-    const [users, setUsers] = useState([])
-
-    useEffect(() => {
-        async function loadUser() {
-            const response = await api.get('user')
-            response.data.sort(function (a, b) {
-                return (a.count < b.count) ? 1 : ((b.count < a.count) ? -1 : 0)
-            })
-            setUsers(response.data)
+class UserView extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: []
         }
+    }
+    async loadUser() {
+        const response = await api.get('user')
+        response.data.sort(function (a, b) {
+            return (a.count < b.count) ? 1 : ((b.count < a.count) ? -1 : 0)
+        })
+        this.setState({
+            users: response.data
+        })
+    }
 
-        loadUser()
-    }, [])
-    return (
-        <>
-            <h1 className='text-center ranking'>Ranking</h1>
-            <table className='table table-stripped table-dark'>
-                <thead>
-                    <th scope="col">Posição</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Acertos</th>
-                </thead>
-                <tbody>
-                    {users.map((user, index) => (
-                        <tr key={user.id}>
-                            <th scope='row'>{index + 1}</th>
-                            <td>{user.name}</td>
-                            <td>{user.count}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </>
-    )
+    componentDidMount() {
+        this.loadUser()
+    }
+
+    render() {
+        return (
+            <>
+                <h1 className='text-center ranking'>Ranking</h1>
+                <table className='table table-stripped table-dark'>
+                    <thead>
+                        <th scope="col">Posição</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Acertos</th>
+                    </thead>
+                    <tbody>
+                        {this.state.users.map((user, index) => (
+                            <tr key={user.id}>
+                                <th scope='row'>{index + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.count}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </>
+        )
+    }
+
 }
 
 export default UserView
